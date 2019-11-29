c2b65bc1e37265d822bd85cfbcaf6e10599c3b2a
mrh9ud <mrh9ud@virginia.edu>
2019-11-29 17:31:07 -0500

    matt's version completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, except: [:index, :create]
+  get '/', to: 'songs#index', as: 'songs'
+  post '/songs', to: 'songs#create'
 end
